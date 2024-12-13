cdb81f2229583e5e9f1dc0ebc2e6456790ac25d9
İbrahim Bolat <bolat6606@hotmail.com>
2024-12-13 15:21:16 +0300

    13.12.2024ikindi

--- src/main/java/org/example/SpringBootJSFwithJava8.java
+++ src/main/java/org/example/SpringBootJSFwithJava8.java
@@ -1,42 +1,15 @@
 package org.example;
 
 
-import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-
-import javax.faces.webapp.FacesServlet;
 
 
 @SpringBootApplication
-public class SpringBootJSFwithJava8 extends SpringBootServletInitializer {
+public class SpringBootJSFwithJava8 {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJSFwithJava8.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean facesServletRegistraiton() {
-        FacesServlet facesServlet = new FacesServlet();
-        ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean(facesServlet, "*.xhtml" );
-        registration.setLoadOnStartup(1);
-        return registration;
-    }
-    @Bean
-    public ServletContextInitializer servletContextInitializer() {
-        return servletContext -> {
-            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-            servletContext.setInitParameter("javax.faces.DATETIMECONVERTER_DEFAULT_TIMEZONE_IS_SYSTEM_TIMEZONE", Boolean.TRUE.toString());
-        };
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<>(new ConfigureListener());
-    }
 }
