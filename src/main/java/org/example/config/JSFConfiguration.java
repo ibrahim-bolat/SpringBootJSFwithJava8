05e7f4e46af9b5b0ae05d2562c31f5a4a5be8a87
İbrahim Bolat <bolat6606@hotmail.com>
2024-12-13 16:59:57 +0300

    13.12.2024son

---
+++ src/main/java/org/example/config/JSFConfiguration.java
@@ -0,0 +1,26 @@
+package org.example.config;
+
+import com.sun.faces.config.FacesInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JSFConfiguration {
+
+    @Bean
+    public ServletContextInitializer initializer(FacesInitializer facesInitializer) {
+        return servletContext -> {
+            //bu ayar set edilmezse convertDateTime br gün önce tarih alıyor.
+            servletContext.setInitParameter("javax.faces.DATETIMECONVERTER_DEFAULT_TIMEZONE_IS_SYSTEM_TIMEZONE", Boolean.TRUE.toString());
+
+            //faces servleti başlatıyor
+            facesInitializer.onStartup(null, servletContext);
+        };
+    }
+    @Bean
+    public FacesInitializer facesInitializer() {
+        return new FacesInitializer();
+    }
+}
